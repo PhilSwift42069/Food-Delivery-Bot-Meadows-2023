d319bf97e3de9296c1b73c8a3ecb84e3ae05bb08
mdcacuci <121271058+mdcacuci@users.noreply.github.com>
2023-04-26 22:32:16 -0700

    Started Following Arcade Drive Tutorial: Link in Description
    https://frcteam3255.github.io/FRC-Java-Tutorial/

--- Food-Delivery-Bot (Command Bot)/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Food-Delivery-Bot (Command Bot)/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,12 +4,31 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
-  public Drivetrain() {}
+  
+  Talon leftTalon = null;
+  Talon rightTalon = null;
 
+
+  public Drivetrain() {
+    
+    //Talons
+    leftTalon = new Talon(Constants.DRIVETRAIN_LEFT_TALON);
+    rightTalon = new Talon(Constants.DRIVETRAIN_RIGHT_TALON);
+  }
+
+  SpeedControllerGroup leftMotors = null;
+  SpeedControllerGroup rightMotors = null;
+
+  DifferentialDrive differentialDrive = null; 
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
